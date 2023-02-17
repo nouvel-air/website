09e831c776031f439d75807b48855cb0f7cabd33
srosset81 <srosset81@gmail.com>
2023-02-17 12:50:22 +0100

    Fix Markdown component

--- components/text/Markdown.js
+++ components/text/Markdown.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import MarkdownToJSX from 'markdown-to-jsx';
 import P from "./P";
+import SubTitle from "./SubTitle";
 
 const Markdown = (props) => (
   <MarkdownToJSX
     options={{
       overrides: {
         p: P,
-        li: props => (<li><P {...props}></P></li>)
+        h1: props => (<SubTitle {...props} />),
+        h2: props => (<SubTitle {...props} />),
+        h3: props => (<SubTitle {...props} />),
+        li: props => (<li><P fontSize="18px" {...props}></P></li>)
       },
     }}
     {...props}
